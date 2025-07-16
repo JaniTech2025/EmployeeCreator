fb4468cc78e13c63627aff22389ed282cef978b8
JaniTech2025 <suba.suri@gmail.com>
2025-07-16 23:52:54 +1000

    Adds changes for employee archival on delete

--- Backend/employee/src/main/java/com/example/employee/employeearchive/EmployeeArchiveMapper.java
+++ Backend/employee/src/main/java/com/example/employee/employeearchive/EmployeeArchiveMapper.java
@@ -1,16 +1,30 @@
 package com.example.employee.employeearchive;
 
 import com.example.employee.employeedetails.Employee;
-import com.example.employee.contractarchive.ContractArchiveMapper;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring", uses = { ContractArchiveMapper.class })
+@Mapper(componentModel = "spring")
 public interface EmployeeArchiveMapper {
 
-    @Mapping(target = "archivedAt", expression = "java(java.time.LocalDateTime.now())")
-    @Mapping(target = "contractArchives", ignore = true)
-    @Mapping(target = "employee", ignore = true)
+    EmployeeArchiveMapper INSTANCE = Mappers.getMapper(EmployeeArchiveMapper.class);
+
+    @Mappings({
+            @Mapping(target = "fname", source = "firstName"),
+            @Mapping(target = "middle_name", source = "middleName"),
+            @Mapping(target = "last_name", source = "lastName"),
+            @Mapping(target = "email", source = "email"),
+            @Mapping(target = "mobile_number", source = "mobileNumber"),
+            @Mapping(target = "residential_address", source = "residentialAddress"),
+            @Mapping(target = "employee_status", source = "employeeStatus"),
+            @Mapping(target = "created_at", source = "createdAt"),
+            @Mapping(target = "updated_at", source = "updatedAt"),
+            @Mapping(target = "photoUrl", source = "photoUrl"),
+            @Mapping(target = "archivedAt", expression = "java(java.time.LocalDateTime.now())"),
+            @Mapping(target = "contractArchives", ignore = true),
+            @Mapping(target = "employee", ignore = true)
+    })
     EmployeeArchive toArchive(Employee employee);
 }
